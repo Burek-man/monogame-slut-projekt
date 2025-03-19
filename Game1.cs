0f27b2bf48938c21196f439c34cda0530d4a1409
Noah <Alnu07001@utb.vaxjo.se>
2025-03-19 10:05:24 +0100

    11

---
+++ Game1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Monogame._2;
+
+namespace spaceshhoter;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Player player;
+    private Texture2D spaceShip;
+    private Texture2D enemyspaceship;
+    private Texture2D backgrundbild;
+    private Texture2D Ufo;
+    private List<Enemy> enemies = new List<Enemy>();
+    Song theme; 
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+        spaceShip = Content.Load<Texture2D>("spaceship");
+
+        enemyspaceship = Content.Load<Texture2D>("enemyspaceship");
+
+        Ufo = Content.Load<Texture2D>("ufoEnemy");
+
+        backgrundbild = Content.Load<Texture2D>("background-road.png");
+
+        player = new Player(spaceShip,new Vector2(380,350),50);
+
+        enemies.Add(new Enemy(enemyspaceship));
+        
+        theme = Content.Load<Song>("themesong1");
+        MediaPlayer.Play(theme);
+
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        
+
+        player.Update();
+        foreach(Enemy Enemy in enemies){
+            Enemy.Update();
+        }
+
+        enemybulletCollision();
+
+        SpawnEnemy();
+        base.Update(gameTime);
+    }  
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.White);
+
+        _spriteBatch.Begin();
+        Rectangle bgRect = new(0, 0, 800, 600);
+        _spriteBatch.Draw(backgrundbild, bgRect, Microsoft.Xna.Framework.Color.White);
+        player.Draw(_spriteBatch);
+        foreach(Enemy Enemy in enemies)
+        Enemy.Draw(_spriteBatch);
+
+        _spriteBatch.End();
+
+
+
+        base.Draw(gameTime);
+    }
+    private void SpawnEnemy(){
+        Random rand = new Random();
+        int value = rand.Next(1,101);
+        int spawnChancePercent = 2;
+        if(value<=spawnChancePercent) {
+            enemies.Add(new Enemy(enemyspaceship));
+            enemies.Add(new Enemy(Ufo));
+        }
+    }
+
+private void enemybulletCollision(){
+for(int i = 0; i <enemies.Count; i++){
+    for(int j = 0; j <player.Bullets.Count; j++){
+        if(enemies[i].Hitbox.Intersects(player.Bullets[j].Hitbox)){
+            enemies.RemoveAt(i);
+            player.Bullets.RemoveAt(j);
+            i--;  
+        }
+    }
+
+}
+}
+
+}
---
+++ player.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.MediaFoundation;
+
+namespace spaceshhoter
+{
+    public class Player
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        private Rectangle hitbox;
+        private KeyboardState newKstate;
+        
+         private KeyboardState oldkState;
+         private float hp;
+
+        private List<bullet> bullets = new List<bullet>();
+
+        public List<bullet> Bullets{
+            get{return bullets;}
+        }
+        public float Hp{
+            get{return hp;}
+        }
+
+        public Player(Texture2D texture, Vector2 position, int pixelSize){
+            this.texture = texture;
+            this.position = position;
+            hitbox = new Rectangle((int)position.X,(int)position.Y,
+            pixelSize,pixelSize);
+
+        }
+    public void Update(){
+        newKstate = Keyboard.GetState();
+       Move();
+       Shoot();
+        oldkState = newKstate;
+
+       foreach(bullet b in bullets){
+        b.Update();
+       }
+    }
+
+
+    private void Shoot(){
+        
+        if((newKstate.IsKeyDown(Keys.Space) && oldkState.IsKeyUp(Keys.Space)) || newKstate.IsKeyDown(Keys.W)){
+            bullet bullet = new bullet(texture,position); 
+            bullets.Add(bullet);
+        }
+    }
+    private void Move(){
+    
+
+        if(newKstate.IsKeyDown(Keys.A)){
+            position.X -= 1;
+        }
+        else if(newKstate.IsKeyDown(Keys.D)){
+            position.X +=1;
+        }
+        hitbox.Location = position.ToPoint();
+    }
+
+
+
+    public void Draw(SpriteBatch spriteBatch){
+        spriteBatch.Draw(texture,hitbox,Color.White);
+        foreach(bullet b in bullets){
+            b.Draw(spriteBatch);
+        }
+    }
+    }
+}
