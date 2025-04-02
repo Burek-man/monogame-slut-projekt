810936f26a336c6cd1a28253dd31dbd62079ca9f
Noah <Alnu07001@utb.vaxjo.se>
2025-04-02 10:05:10 +0200

    inte klr

--- obj/Debug/net6.0-windows/monogame1.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/monogame1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76201dbdd4fcdf6e45326c400124d2ab0bd07568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ce802ccbe19843cee584820d5761ca8ef63bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
