927d343d1af785b3641ad43f00e924cdefd5b2ec
Noah <Alnu07001@utb.vaxjo.se>
2025-04-25 13:59:17 +0200

    aa

--- obj/Debug/net6.0-windows/monogame1.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/monogame1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff9b6967dc97c4ecf3a27746ff7fa5ab839d457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5bb3939b9f5d8fdfb0369e936aad0d91fa52f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
